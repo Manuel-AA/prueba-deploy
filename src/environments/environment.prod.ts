f27005336c8bea3ec899b56a8bd7e38428676d91
mAnuel-AA <100383545@alumnos.uc3m.es>
2020-05-05 00:51:58 +0200

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  /*firebaseConfig: {
+    apiKey: "AIzaSyAnUHFard4vFvo7OvezS-sMT_Gkg2xYzJI",
+    authDomain: "prueba-deploy-c5784.firebaseapp.com",
+    databaseURL: "https://prueba-deploy-c5784.firebaseio.com",
+    projectId: "prueba-deploy-c5784",
+    storageBucket: "prueba-deploy-c5784.appspot.com",
+    messagingSenderId: "1063854408256",
+    appId: "1:1063854408256:web:beda7aa8e89bd68b8955a5",
+    measurementId: "G-24JBT0WXNG"
+  }*/
 };
